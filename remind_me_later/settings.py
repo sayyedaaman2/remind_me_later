fe02ccf111324359ccedc9134f18dc003c8ef587
PrinceAaman <98977020+sayyedaaman2@users.noreply.github.com>
2024-03-12 03:54:10 +0000

    testing on code sand box

--- remind_me_later/settings.py
+++ remind_me_later/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5snm%!-g42gs+@9rtrkl)py(bbu0**z=r-o#__mb2=m^r##pq!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'remind_me_later.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,"templates"],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR,'static'
+    BASE_DIR, 'static'
 ]
-
-
